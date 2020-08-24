2649199481302768bb634532490162ef9a0aa7d2
amankumar6 <62713154+amankumar6@users.noreply.github.com>
2020-08-24 08:58:16 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+var input = document.querySelectorAll('textarea')[0],
+    characterCount = document.querySelector('#characterCount'),
+    wordCount = document.querySelector('#wordCount');
+
+input.addEventListener('keyup', function () {
+    console.clear();
+    characterCount.innerHTML = input.value.length;
+    var words=input.value.match(/\w+/g);
+    if (words) {
+        wordCount.innerHTML = words.length;
+    } else {
+        wordCount.innerHTML = 0;
+    }
+});
