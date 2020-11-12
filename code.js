d969964d07a8836df2c7b32cbb28ec45c0c1c6e1
amankumar6 <amankumar786dpsdh@gmail.com>
2020-11-12 22:53:38 +0530

    style: ui styling + feat: pug and sass has been used,Reset button has been added

--- code.js
+++ code.js
@@ -1,14 +1,22 @@
-var input = document.querySelectorAll('textarea')[0],
+const input = document.querySelector('textarea'),
     characterCount = document.querySelector('#characterCount'),
-    wordCount = document.querySelector('#wordCount');
+    wordCount = document.querySelector('#wordCount'),
+    btn = document.querySelector('.btn'),
+    label = document.querySelector('.label');
 
-input.addEventListener('keyup', function () {
-    console.clear();
+input.addEventListener('keyup', () => {
     characterCount.innerHTML = input.value.length;
-    var words=input.value.match(/\w+/g);
+    let words = input.value.match(/\w+/g);
     if (words) {
         wordCount.innerHTML = words.length;
     } else {
         wordCount.innerHTML = 0;
     }
 });
+
+btn.addEventListener('click', () => {
+    characterCount.innerHTML = 0;
+    wordCount.innerHTML = 0;
+    input.value = null;
+    M.toast({html: 'Reset'})
+})
