13741d5859bcf0dcc11fdba843a67651b899e8be
amankumar6 <amankumar786dpsdh@gmail.com>
2021-06-19 20:30:45 +0530

    style: style update

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 let input = document.querySelector('textarea'),
     characterCount = document.querySelector('#characterCount'),
-    wordCount = document.querySelector('#wordCount');
+    wordCount = document.querySelector('#wordCount'),
+    toastHTML = `<span style="font-family: 'Nunito', sans-serif ">Cleared</span>`;
 
 input.addEventListener('keyup', () => {
     characterCount.innerHTML = input.value.length;
@@ -9,9 +10,10 @@ input.addEventListener('keyup', () => {
 });
 
 document.querySelector('.btn').addEventListener('click', () => {
+    if (input.value == 0) return;
     characterCount.innerHTML = 0;
     wordCount.innerHTML = 0;
     input.value = null;
     input.style.height = '45px';
-    M.toast({ html: 'Reset' });
+    M.toast({ html: toastHTML });
 });
